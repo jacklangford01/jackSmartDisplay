988948ee17a49090321cb66dddc707156d228830
[Jack Langford] <[jacklangford2004@gmail.com]>
2026-07-22 16:12:12 -0700

    More Bug Fixes

--- kiosk.sh
+++ kiosk.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Disable xset blanking, let xscreensaver handle that.
-xset s noblank
-xset s off
-xset -dpms
-
-# Start xscreensaver.
-xscreensaver &
-
 # TODO: Set this to a default, but allow easy override by passing a parameter or
 # using the environment variable KIOSK_URL.
-KIOSK_URL="http://localhost:3000"
+KIOSK_URL="${KIOSK_URL:-http://localhost:3000}"
 
 # Wait for services to come online.
 # TODO: It would be nice to get rid of this, but right now on Bookworm, if we
@@ -18,6 +10,23 @@ KIOSK_URL="http://localhost:3000"
 sleep 8
 
 echo 'Starting Chromium...'
+
+if command -v chromium >/dev/null 2>&1; then
+    CHROMIUM_BIN="$(command -v chromium)"
+elif command -v chromium-browser >/dev/null 2>&1; then
+    CHROMIUM_BIN="$(command -v chromium-browser)"
+else
+    echo 'Chromium executable not found.' >&2
+    exit 127
+fi
+
 # Replace this shell with Chromium so systemd tracks crashes and OOM kills
 # directly and can restart the kiosk reliably.
-exec /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --app="$KIOSK_URL"
+exec "$CHROMIUM_BIN" \
+    --ozone-platform=wayland \
+    --noerrdialogs \
+    --disable-infobars \
+    --disable-session-crashed-bubble \
+    --no-first-run \
+    --kiosk \
+    --app="$KIOSK_URL"
